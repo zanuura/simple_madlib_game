99eb13e11b707886d65c0193f89b44dc2f9ad246
Hammam Hanan <73764446+zanuura@users.noreply.github.com>
2023-09-28 14:15:11 +0700

    first commit

---
+++ simple_mad_lib_game/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace MadLibGame
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("\n------------------------------\n");
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
+
+            Console.ReadLine();
+        }
+    }
+}
